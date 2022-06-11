b7153e3523617c6e19d1e3aea865329176a05a17
Whit Morgan <webdevwhit@gmail.com>
2022-06-10 20:49:59 -0400

    updated server, ejs, and main

--- public/js/main.js
+++ public/js/main.js
@@ -2,23 +2,23 @@ const deleteText = document.querySelectorAll('.fa-trash')
 const thumbText = document.querySelectorAll('.fa-thumbs-up')
 
 Array.from(deleteText).forEach((element)=>{
-    element.addEventListener('click', deleteRapper)
+    element.addEventListener('click', deleteCartridge)
 })
 
 Array.from(thumbText).forEach((element)=>{
     element.addEventListener('click', addLike)
 })
 
-async function deleteRapper(){
-    const sName = this.parentNode.childNodes[1].innerText
-    const bName = this.parentNode.childNodes[3].innerText
+async function deleteCartridge(){
+    const gType = this.parentNode.childNodes[1].innerText
+    const cName = this.parentNode.childNodes[3].innerText
     try{
-        const response = await fetch('deleteRapper', {
+        const response = await fetch('deleteCartridge', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-              'stageNameS': sName,
-              'birthNameS': bName
+              'gunTypeS': gType,
+              'cartridgeNameS': cName
             })
           })
         const data = await response.json()
@@ -31,16 +31,16 @@ async function deleteRapper(){
 }
 
 async function addLike(){
-    const sName = this.parentNode.childNodes[1].innerText
-    const bName = this.parentNode.childNodes[3].innerText
+    const gType = this.parentNode.childNodes[1].innerText
+    const cName = this.parentNode.childNodes[3].innerText
     const tLikes = Number(this.parentNode.childNodes[5].innerText)
     try{
         const response = await fetch('addOneLike', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-              'stageNameS': sName,
-              'birthNameS': bName,
+              'gunTypeS': gType,
+              'cartridgeNameS': cName,
               'likesS': tLikes
             })
           })
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'rap'
+    dbName = 'cartridgeLoads'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
@@ -22,25 +22,25 @@ app.use(express.json())
 
 
 app.get('/',(request, response)=>{
-    db.collection('rappers').find().sort({likes: -1}).toArray()
+    db.collection('cartridges').find().sort({likes: -1}).toArray()
     .then(data => {
         response.render('index.ejs', { info: data })
     })
     .catch(error => console.error(error))
 })
 
-app.post('/addRapper', (request, response) => {
-    db.collection('rappers').insertOne({stageName: request.body.stageName,
-    birthName: request.body.birthName, likes: 0})
+app.post('/addCartridge', (request, response) => {
+    db.collection('cartridges').insertOne({gunType: request.body.gunType,
+    cartridgeName: request.body.cartridgeName, likes: 0})
     .then(result => {
-        console.log('Rapper Added')
+        console.log('Cartridge Added')
         response.redirect('/')
     })
     .catch(error => console.error(error))
 })
 
 app.put('/addOneLike', (request, response) => {
-    db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
+    db.collection('cartridges').updateOne({gunType: request.body.gunTypeS, cartridgeName: request.body.cartridgeNameS, likes: request.body.likesS},{
         $set: {
             likes:request.body.likesS + 1
           }
@@ -56,11 +56,11 @@ app.put('/addOneLike', (request, response) => {
 
 })
 
-app.delete('/deleteRapper', (request, response) => {
-    db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
+app.delete('/deleteCartridge', (request, response) => {
+    db.collection('cartridges').deleteOne({cartridgeName: request.body.cartridgeNameS})
     .then(result => {
-        console.log('Rapper Deleted')
-        response.json('Rapper Deleted')
+        console.log('Cartridge Deleted')
+        response.json('Cartridge Deleted')
     })
     .catch(error => console.error(error))
 
